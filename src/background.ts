8cf615f394e4902d136d214a1a1484b4bc425475
Ethan Leisinger <packruler@gmail.com>
2022-07-31 16:22:53 -0600

    Add .editorconfig

--- src/background.ts
+++ src/background.ts
@@ -1,9 +1,6 @@
 chrome.action.onClicked.addListener((tab) => {
-  let currentState;
   chrome.storage.local.get(['enabled'], function ({ enabled: enabled }) {
     console.log('Value currently is ' + enabled);
-    currentState = enabled ?? false;
-    console.log("Toggle dark mode");
     chrome.storage.local.set({ enabled: !enabled });
   });
 });
