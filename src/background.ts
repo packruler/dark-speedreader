cf9bf1e90094add30ed7aa59473164e8630a52a6
Ethan Leisinger <packruler@gmail.com>
2022-07-31 18:30:03 -0600

    Enable the ability to toggle dark mode state

--- src/background.ts
+++ src/background.ts
@@ -1,7 +1,21 @@
-chrome.action.onClicked.addListener((tab) => {
-  chrome.storage.local.get(['enabled'], function ({ enabled: enabled }) {
-    console.log('Value currently is ' + enabled);
+const darkIconPath = 'icons/icon_dark_16.png';
+const lightIconPath = 'icons/icon_light_16.png';
 
-    chrome.storage.local.set({ enabled: !enabled });
+function updateAction(enabled: boolean) {
+  chrome.action.setIcon({ path: enabled ? darkIconPath : lightIconPath });
+  chrome.action.setTitle({ title: enabled ? 'Disable Dark Mode' : 'Enable Dark Mode' });
+}
+
+chrome.action.onClicked.addListener(() => {
+  chrome.storage.local.get(['enabled'], function ({ enabled: enabled }) {
+    enabled = !enabled;
+
+    chrome.storage.local.set({ enabled: enabled });
+
+    updateAction(enabled);
   });
+});
+
+chrome.storage.local.get(['enabled'], ({ enabled: enabled }) => {
+  updateAction(enabled);
 });
